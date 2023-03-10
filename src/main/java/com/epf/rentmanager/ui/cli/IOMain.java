e3df9dadac3e36cfbe229c3fa334ab4be0edde59
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-03-10 09:54:30 +0100

    retravail IOMain

--- src/main/java/com/epf/rentmanager/ui/cli/IOMain.java
+++ src/main/java/com/epf/rentmanager/ui/cli/IOMain.java
@@ -3,71 +3,69 @@ package com.epf.rentmanager.ui.cli;
 import com.epf.rentmanager.utils.IOUtils;
 
 public class IOMain {
-	private static boolean quit = false;
+	private static boolean fin = false;
 
 	public static void main(String[] args) {
-		System.out.println("Bienvenue dans l'application.");
-		while (!quit) {
-			displayMenu();
+		System.out.println("Bonjour");
+		while (!fin) {
+			afficherMenu();
 		}
 	}
 
-	public static void displayMenu() {
+	public static void afficherMenu() {
 		IOUtils.print("Que voulez-vous faire ?");
 		IOUtils.print("""
-                    [1] Lister des enregistrements
-                    [2] Créer des enregistrements
-                    [3] Quitter le programme""");
+		[1] Afficher les enregistrements
+		[2] Créer des enregistrements
+		[3] Quitter le programme""");
 
-		int choice = IOUtils.readInt("Entrez votre choix : ");
-		switch (choice) {
-			case 1 -> displayListOptions();
+		int choix = IOUtils.readInt("Entrez votre choix : ");
+		switch (choix) {
+			case 1 -> afficherOptions();
 			case 2 -> displayCreateOptions();
 			case 3 -> {
 				IOUtils.print("Au revoir !");
-				quit = true;
+				fin = true;
 			}
-			default -> IOUtils.print("Option invalide.");
+			default -> IOUtils.print("Le choix [" + choix + "] n'est pas reconnu");
 		}
 	}
 
-	public static void displayListOptions() {
+	public static void afficherOptions() {
 		IOUtils.print("""
-                    [1] Lister les clients
-                    [2] Lister les véhicules
-                    [3] Lister les réservations
-                    [4] Quitter le programme""");
-
-		int choice = IOUtils.readInt("Entrez votre choix : ");
-		switch (choice) {
+		[1] Lister les clients
+		[2] Lister les véhicules
+		[3] Lister les réservations
+		[4] Quitter le programme""");
+		int choix = IOUtils.readInt("Entrez votre choix : ");
+		switch (choix) {
 			case 1 -> IOClient.listClients();
 			case 2 -> IOVehicle.listVehicles();
 			case 3 -> IOReservation.listReservations();
 			case 4 -> {
 				IOUtils.print("Au revoir !");
-				quit = true;
+				fin = true;
 			}
-			default -> IOUtils.print("Option invalide.");
+			default -> IOUtils.print("Le choix [" + choix + "] n'est pas reconnu");
 		}
 	}
 
 	public static void displayCreateOptions() {
 		IOUtils.print("""
-                    [1] Créer un client
-                    [2] Créer un véhicule
-                    [3] Créer une réservation
-                    [4] Quitter le programme""");
-
-		int choice = IOUtils.readInt("Entrez votre choix : ");
-		switch (choice) {
+		[1] Créer un client
+		[2] Créer un véhicule
+		[3] Créer une réservation
+		[4] Quitter le programme""");
+		int choix = IOUtils.readInt("Entrez votre choix : ");
+		switch (choix) {
 			case 1 -> IOClient.createClient();
 			case 2 -> IOVehicle.createVehicle();
 			case 3 -> IOReservation.createReservation();
 			case 4 -> {
 				IOUtils.print("Au revoir !");
-				quit = true;
+				fin = true;
 			}
-			default -> IOUtils.print("Option invalide.");
+			default -> IOUtils.print("Le choix [" + choix + "] n'est pas reconnu");
 		}
 	}
 }
