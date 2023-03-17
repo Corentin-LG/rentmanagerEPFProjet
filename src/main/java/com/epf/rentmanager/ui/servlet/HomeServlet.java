6a4b8d58280cacc81ea79cf42e918785c894791e
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-03-17 10:42:30 +0100

    HomeServlet autwired fonctionennt

--- src/main/java/com/epf/rentmanager/ui/servlet/HomeServlet.java
+++ src/main/java/com/epf/rentmanager/ui/servlet/HomeServlet.java
@@ -7,7 +7,9 @@ import com.epf.rentmanager.exception.ServiceException;
 import com.epf.rentmanager.service.ClientService;
 import com.epf.rentmanager.service.ReservationService;
 import com.epf.rentmanager.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,10 +25,22 @@ public class HomeServlet extends HttpServlet {
      *Mapper, Try(ressources)
      */
     private static final long serialVersionUID = 1L;
-    private ClientService clientService = new ClientService(new ClientDao());
-    private VehicleService vehicleService = new VehicleService(new VehicleDao());
-    private ReservationService reservationService = new ReservationService(new ReservationDao());
+//    private ClientService clientService = new ClientService(new ClientDao());
+//    private VehicleService vehicleService = new VehicleService(new VehicleDao());
+//    private ReservationService reservationService = new ReservationService(new ReservationDao());
 
+    @Autowired
+    ClientService clientService;
+    @Autowired
+    VehicleService vehicleService;
+    @Autowired
+    ReservationService reservationService;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
